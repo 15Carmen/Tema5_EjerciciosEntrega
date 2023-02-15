5bb00987cea74f51ad97632f1495c6486665a449
15Carmen <carmen.martin@iesnervion.es>
2023-02-15 13:18:58 +0100

    7 y media modificada

--- src/ejercicios/sieteMedia/Main.java
+++ src/ejercicios/sieteMedia/Main.java
@@ -4,32 +4,32 @@ public class Main {
     public static void main(String[] args) {
 
         //Declaramos las variables
-        double puntosJ1=0;//donde guardamos los puntos del jugador 1
-        double puntosJ2=0;//donde guardamos los puntos del jugador 2
+        double puntosJ1 = 0;    //Variable donde guardaremos los puntos del jugador 1
+        double puntosJ2 = 0;    //Variable donde guardaremos los puntos del jugador 2
 
-        //actualizamos los puntos del jugador uno segun los puntos que nos salgan en las tiradas
-        puntosJ1=Utilidades.turno("Jugador 1", puntosJ1);
+        //Actualizaremos los puntos cada jugador según los puntos que nos salgan en las tiradas
+        puntosJ1 = Utilidades.turno("Jugador 1", puntosJ1);
+        puntosJ2 = Utilidades.turno("Jugador 2", puntosJ2);
 
-        if (puntosJ1>7.5){ //En caso de que se pasen de 7,5 automáticamente perdería el jugador 1
-            System.out.println("Jugador 1 ha perdido, enhorabuena Jugador 2.");
-        }
-        else {  //si no es el caso actualizamos los puntos del jugador 2
-            puntosJ2=Utilidades.turno("Jugador 2", puntosJ2);
-
-            if (puntosJ2>7.5){  //si el jugador 2 se pasa de 7,5 ganara el jugador 1
-                System.out.println("Jugador 2 ha perdido, enhorabuena Jugador 1.");
-            }else { //si ninguno se pasase comprobamos quien tiene mayor puntuacion, es decir, quien se acercaria mas a 7,5 y se declarara ganador
-                if (puntosJ1>puntosJ2){
-                    System.out.println("Jugador 2 ha perdido, enhorabuena Jugador 1.");
-                } else if (puntosJ1<puntosJ2) {
-                    System.out.println("Jugador 1 ha perdido, enhorabuena Jugador 2.");
-                }
-                else { //en caso de que ambos tengan misma puntuacion se declara empate
-                    System.out.println("Empate.");
+        if (puntosJ1>7.5){  //Si el jugador 1 se pasa de 7.5
+            if (puntosJ2>7.5){ //Si jugador 2 tambien se pasa declaramos empate
+                System.out.println("Empate, ambos os habeis pasado");
+            }else{ //Si jugador 2 no se pasa, jugador 2 gana
+                System.out.println("Enhorabuena, has ganado Jugador 2");
+            }
+        }else{ //Si jugador 1 no se pasa
+            if (puntosJ2>7.5){ //Si jugador 2 se pasa, gana el jugador 1
+                System.out.println("Enhorabuena, has ganado Jugador 1");
+            }else{ //Si jugador 2 no se pasa, comparamos las puntuaciones de ambos
+                if (puntosJ1 > puntosJ2) { //Si los puntos de J1 son más que los de J2, gana J1
+                    System.out.println("Enhorabuena, has ganado Jugador 1");
+                } else if (puntosJ1 < puntosJ2) { //Si los puntos de J2 son más que los de J2, gana J2
+                    System.out.println("Enhorabuena, has ganado Jugador 2");
+                } else if (puntosJ1==puntosJ2){  //en caso de que ambos tengan misma puntuación se declara empate
+                    System.out.println("Empate, ambos teneis la misma puntuacion");
                 }
             }
         }
-
 
     }
 
